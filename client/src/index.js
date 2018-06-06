5409071f40188065df0183e42af029214f55c81f
Taron Musyelyan <mutaron@yahoo.com>
2018-06-06 17:43:12 +0400

    commit brfore running eject

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+const app = (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
